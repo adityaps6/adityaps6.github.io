505fdad433805e7cac3ea39cb702492b86a8219b
adityaps6 <131631810+adityaps6@users.noreply.github.com>
2025-07-24 00:15:12 -0400

    minor changes

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -197,7 +197,7 @@ const CONFIG = {
     ],
   },
 
-  footer: `Made with <a class="text-primary" href="https://github.com/arifszn/gitprofile" target="_blank" rel="noreferrer">GitProfile</a> and ❤️`,
+  footer: `Made with ❤️`,
   enablePWA: true,
 };
 
